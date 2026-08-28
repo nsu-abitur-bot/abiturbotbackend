7c30146edbaae34f1b2925b77f54f431f8ec92af
colder159 <qwezitrakspsiblade@gmail.com>
2026-08-28 18:55:03 +0700

    feat: add main application class for AbiturBot

---
+++ src/main/java/ru/nsu/abitur/bot/AbiturBotApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.abitur.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbiturBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbiturBotApplication.class, args);
+	}
+
+}
