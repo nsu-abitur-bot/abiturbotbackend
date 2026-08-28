895e71ed3e566b81465dcf93051cbfa74e93df27
colder159 <qwezitrakspsiblade@gmail.com>
2026-08-28 18:55:08 +0700

    feat: add application test class for AbiturBot

---
+++ src/test/java/ru/nsu/abitur/bot/AbiturBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.abitur.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbiturBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
