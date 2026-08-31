3ea0cc68623a42d5a58d2da554488c91463c70b4
colder159 <qwezitrakspsiblade@gmail.com>
2026-08-31 19:15:59 +0700

    feat: update Client entity to include Lombok annotations and improve documentation

--- src/main/java/ru/nsu/abitur/bot/core/model/Client.java
+++ src/main/java/ru/nsu/abitur/bot/core/model/Client.java
@@ -1,24 +1,27 @@
 package ru.nsu.abitur.bot.core.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.Instant;
+import java.util.List;
 
 @Entity
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 /**
- * Сущность для хранения пользователя в БД.
+ * Сущность для хранения клиента бота в БД.
  */
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     @Column(unique = true)
     private Long telegramId;
     @Column(unique = true)
